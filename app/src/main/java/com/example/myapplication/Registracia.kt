0aa6da2c93ccec33e7bc50bdc70340a93b35fd88
davit <daviti.gogilashvili.1@btu.edu.ge>
2023-01-24 16:47:27 +0400

    ვვსს

---
+++ app/src/main/java/com/example/myapplication/Registracia.kt
@@ -0,0 +1,55 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+
+class Registracia:Fragment(R.layout.registracia) {
+    private lateinit var Email:EditText
+    private lateinit var PASSWORD :EditText
+    private lateinit var buttuon3 : Button
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view=inflater.inflate(R.layout.registracia,container,false)
+        Email=view.findViewById(R.id.Email)
+        PASSWORD=view.findViewById(R.id.PASSWORD)
+        buttuon3=view.findViewById(R.id.button3)
+        
+        buttuon3.setOnClickListener { 
+            var password=PASSWORD.text.toString()
+            var email=Email.text.toString()
+            
+            if (password.isNotEmpty()&&email.isNotEmpty()){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                    .addOnSuccessListener {
+                        Toast.makeText(this.requireContext(), "წარმსტებით დარეგისტრილდით", Toast.LENGTH_SHORT).show()
+                    }
+                
+                
+                
+            }
+        }
+        
+        
+        
+        
+
+        return view
+
+
+
+
+    }
+}
